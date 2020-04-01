437594f2b90063e5646d277bca0a28b995b7bd3b
Gia Edgington <gia.edgington@gmail.com>
2020-03-31 20:29:37 -0400

    Prepopulate edit product page

--- controllers/admin.js
+++ controllers/admin.js
@@ -3,7 +3,8 @@ const Product = require('../models/product');
 exports.getAddProduct = (req, res, next) => {
     res.render('admin/edit-product', 
     { pageTitle: 'Add Product', 
-    path: '/admin/add-product'
+    path: '/admin/add-product',
+    editing: false
     });
 };
 
@@ -12,7 +13,6 @@ exports.postAddProduct = (req, res, next) => {
     const imageUrl = req.body.imageUrl;
     const price = req.body.price;
     const description = req.body.description;
-
     const product = new Product(title, imageUrl, price, description);
     product.save();
     res.redirect('/');
@@ -23,10 +23,17 @@ exports.getEditProduct = (req, res, next) => {
     if (!editMode) {
         return res.redirect('/');
     }
-    res.render('admin/edit-product', 
-    { pageTitle: 'Edit Product', 
-    path: '/admin/edit-product',
-    editing: editMode
+    const prodId = req.params.productId;
+    Product.findById(prodId, product => {
+        if (!product) {
+            return res.redirect('/');
+        }
+        res.render('admin/edit-product', 
+        { pageTitle: 'Edit Product', 
+        path: '/admin/add-product',
+        editing: editMode,
+        product: product
+        });
     });
 };
 
